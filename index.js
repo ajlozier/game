f98056bcc82c00ccafbd871855afe402e998c260
Aaron Lozier <lozieraj@gmail.com>
2020-03-22 19:11:02 -0600

    Ability to look at objects

--- index.js
+++ index.js
@@ -10,14 +10,15 @@ let currentRoomId;
 console.log(`Welcome to ${config.gameName}!`);
 console.log(config.directions);
 
-rl.question("What is your name ? ", function(name) {
+rl.question("What is your name ? ", function (name) {
     currentRoomId = config.startingRoomId;
     showRoom()
     //rl.close();
 });
 
-rl.on("line", function(command) {
-    switch(command) {
+rl.on("line", function (command) {
+    const parts = command.split(" ");
+    switch (parts[0]) {
         case "n":
         case "s":
         case "e":
@@ -30,23 +31,45 @@ rl.on("line", function(command) {
         case "quit":
             rl.close();
             break;
+        case "lookat":
+            lookatObject(parts[1]);
+            break;
         default:
             console.log(`Unrecognized command: ${command}`);
             break;
     }
 });
 
-rl.on("close", function() {
+rl.on("close", function () {
     console.log("\nGAME OVER");
     process.exit(0);
 });
 
 // Functions
 
+function lookatObject(objectName) {
+    const obje = getObjectByName(objectName);
+    if (obje) {
+        console.log(obje.description);
+    } else {
+        console.log(`There is no ${objectName} in this room`);
+    }
+}
+
+function getObjectByName(objectName) {
+    const room = getRoomById(currentRoomId);
+    for (let i = 0; i < room.objects.length; i++) {
+        if (room.objects[i].name === objectName) {
+            return room.objects[i];
+        }
+    }
+    return false;
+}
+
 function moveDirection(direction) {
     const room = getRoomById(currentRoomId);
     const directions = getRoomDirections(room);
-    if(directions.indexOf(direction) === -1) {
+    if (directions.indexOf(direction) === -1) {
         console.log(`You can't go that way.`);
     } else {
         currentRoomId = room.adjacent[direction];
@@ -55,8 +78,8 @@ function moveDirection(direction) {
 }
 
 function getRoomById(roomId) {
-    for(let i = 0; i < config.rooms.length; i++) {
-        if(config.rooms[i].id === roomId) {
+    for (let i = 0; i < config.rooms.length; i++) {
+        if (config.rooms[i].id === roomId) {
             return config.rooms[i];
         }
     }
